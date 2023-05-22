1a70f01263d7fe6ad6b14e644e4dd0890c3f428a
Leo <sed@interia.eu>
2023-05-23 00:02:31 +0200

    part two

--- advent-of-code/2021/day-1/day-1/src/main/java/PartOne.java
+++ advent-of-code/2021/day-1/day-1/src/main/java/PartOne.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
-public class Solution {
+public class PartOne {
 
     public static void main(String[] args) throws IOException {
 
